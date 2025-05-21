d7efa9da86114ca072b543be80588bdf5c5fe85f
Ferhan BAYIR <ferhanbayir1@gmail.com>
2025-05-21 10:57:33 +0300

    fix.

--- lib/main.dart
+++ lib/main.dart
@@ -16,7 +16,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      title: 'OMDB App',
+      title: 'Notflix',
       home: const BottomBar(),
     );
   }
